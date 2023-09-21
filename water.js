fe499c70c2be421aa7548730b7819378de236c76
Suzoka/Morgan <mor9an77@gmail.com>
2023-09-21 10:18:25 +0200

    :construction: work in progress, 09-21-2023

--- water.js
+++ water.js
@@ -12,56 +12,53 @@ document.querySelectorAll('div.element').forEach(function (element) {
 });
 
 
-//Si la page est bien entièrement chargée
-window.addEventListener("DOMContentLoaded", function () {
-    let position = 0;
-    const droite = document.querySelector('.sliderDroite')
-    const gauche = document.querySelector('.sliderGauche')
-    const slider = document.querySelector('.flexbox')
+let position = 0;
+const droite = document.querySelector('.sliderDroite')
+const gauche = document.querySelector('.sliderGauche')
+const slider = document.querySelector('.flexbox')
 
-    //Si on clique sur la flèche de droite, on décale le slider vers la gauche
-    droite.addEventListener('click', function () {
-        position++;
-        decaleGauche();
-    })
+//Si on clique sur la flèche de droite, on décale le slider vers la gauche
+droite.addEventListener('click', function () {
+    position++;
+    decaleGauche();
+})
 
-    //Si on clique sur la flèche de gauche, on décale le slider vers la droite
-    gauche.addEventListener('click', function () {
-        position--;
-        decaleDroite();
-    })
+//Si on clique sur la flèche de gauche, on décale le slider vers la droite
+gauche.addEventListener('click', function () {
+    position--;
+    decaleDroite();
+})
 
 
 
-    function decaleGauche() {
-        //Si on arrive à la fin du slider, on revient au début en coupant l'animation (slider infini)
-        if (position == 4) {
-            slider.style.transition = "0s";
-            slider.style.left = "-22.5%";
-            setTimeout(function () {
-                slider.style.transition = "1s";
-                slider.style.left = "-68%";
-                position = 1;
-            }, 100);
-            return
-        }
-        //Sinon on décale simplement le slider
-        slider.style.left = "-" + ((position) * 47 + 22.5) + "%";
+function decaleGauche() {
+    //Si on arrive à la fin du slider, on revient au début en coupant l'animation (slider infini)
+    if (position == 4) {
+        slider.style.transition = "0s";
+        slider.style.left = "-22.5%";
+        setTimeout(function () {
+            slider.style.transition = "1s";
+            slider.style.left = "-68%";
+            position = 1;
+        }, 100);
+        return
     }
+    //Sinon on décale simplement le slider
+    slider.style.left = "-" + ((position) * 47 + 22.5) + "%";
+}
 
-    //Si on arrive au début du slider, on revient à la fin en coupant l'animation (slider infini)
-    function decaleDroite() {
-        if (position == -1) {
-            slider.style.transition = "0s";
-            slider.style.left = "-163.5%";
-            setTimeout(function () {
-                slider.style.transition = "1s";
-                slider.style.left = "-116.5%";
-                position = 2;
-            }, 100);
-            return
-        }
-        //Sinon on décale simplement le slider
-        slider.style.left = "-" + ((position) * 47 + 22.5) + "%";
+//Si on arrive au début du slider, on revient à la fin en coupant l'animation (slider infini)
+function decaleDroite() {
+    if (position == -1) {
+        slider.style.transition = "0s";
+        slider.style.left = "-163.5%";
+        setTimeout(function () {
+            slider.style.transition = "1s";
+            slider.style.left = "-116.5%";
+            position = 2;
+        }, 100);
+        return
     }
-});
+    //Sinon on décale simplement le slider
+    slider.style.left = "-" + ((position) * 47 + 22.5) + "%";
+}
