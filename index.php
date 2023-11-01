80b38c579a4d351e9b4cdbb5ec955411b5a95113
Suzoka/Morgan <mor9an77@gmail.com>
2023-11-01 17:08:27 +0100

    :speech_balloon: add projects

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Rubik:wght@300;500&display=swap"
         rel="stylesheet">
+    <link rel="icon" href="./img/filigrane.svg" type="image/svg+xml">
+</head>
 
 <body>
 
@@ -26,7 +28,7 @@
             <a href="" title="" class="logo"><img src="./img/filigrane.svg" alt="Accueil"></a>
             <nav>
                 <a href="" title="">Qui suis-je ?</a>
-                <a href="" title="">Mes projets</a>
+                <a href="./projets.php" title="Mes projets">Mes projets</a>
                 <a href="" title="">Contact</a>
             </nav>
         </header>
@@ -41,18 +43,23 @@
             </div>
             <section class="content">
                 <h2>Bienvenue sur mon portfolio !</h2>
-                <p>Je m'appel Morgan, et suis étudiant en <span class="bold">BUT MMI</span>. Dans cette filière, j'ai choisis de suivre le parcours <span class="bold">"Développement Web et dispositifs intéractifs"</span>. Vous trouverez dans ce portfolio différents de mes projets, qu'ils soient personnels ou scolaire. <br><br> Avec ce portfolio, vous avez un moyen de plonger dans ce que je suis capable de faire ! <br><br>J'espère sincèrement que cette exploration à travers mon univers vous plaira.</p>
+                <p>Je m'appel Morgan, et suis étudiant en <span class="bold">BUT MMI</span>. Dans cette filière, j'ai
+                    choisis de suivre le parcours <span class="bold">"Développement Web et dispositifs
+                        intéractifs"</span>. Vous trouverez dans ce portfolio différents de mes projets, qu'ils soient
+                    personnels ou scolaire. <br><br> Avec ce portfolio, vous avez un moyen de plonger dans ce que je
+                    suis capable de faire ! <br><br>J'espère sincèrement que cette exploration à travers mon univers
+                    vous plaira.</p>
                 <a href="" class="linkAlone">En savoir plus sur moi</a>
             </section>
             <h2 class="projet">Quelques-uns de mes projets :</h2>
             <div class="hide">
                 <div class="flexbox">
                     <?php
-                    $projets = getProjets();
+                    $projets = getSlider();
                     foreach ($projets as $row) {
                         ?>
                         <a class="lienProjet" id="<?= $row["id_projet"] ?>" href="./projet.php?id=<?= $row["id_projet"] ?>">
-                            <img src="./img/projet/<?= $row["id_projet"] ?>.png" alt="" width="150px" class="logo">
+                            <img src="./img/<?= $row["url_img"] ?>" alt="" width="130px" class="logo">
                             <div class="text">
                                 <h3>
                                     <?= $row["nom_projet"] ?>
--- script/script.php
+++ script/script.php
@@ -1,18 +1,36 @@
 <?php
 include("database.php");
 
-function getProjets (){
-global $db;
-$stmt = $db->prepare("SELECT * FROM `projets`");
-$stmt->execute();
-return  $stmt->fetchAll(PDO::FETCH_ASSOC);
+function getProjets()
+{
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM `projets`");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function getProjetsById($id)
+{
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM `projets` WHERE `id_projet` = :id");
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
-function getTechnos ($id){
-global $db;
-$stmt = $db->prepare('SELECT * FROM `technologies` t INNER JOIN `moyens` m ON t.id_techno = m.ext_id_techno WHERE m.ext_id_projet ='.$id.';');
-$stmt->execute();
-return  $stmt->fetchAll(PDO::FETCH_ASSOC);
+function getSlider()
+{
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM `projets` WHERE `note_perso`>3 ORDER BY RAND() LIMIT 6");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getTechnos($id)
+{
+    global $db;
+    $stmt = $db->prepare('SELECT * FROM `technologies` t INNER JOIN `moyens` m ON t.id_techno = m.ext_id_techno WHERE m.ext_id_projet =' . $id . ';');
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
